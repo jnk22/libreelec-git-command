0a91137589da6bf59f06425e485046d5b0ac8286
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2022-06-13 17:56:59 +0200

    Fix install script

--- install-git.sh
+++ install-git.sh
@@ -3,36 +3,39 @@
 
 REPO_FILE=https://github.com/jnk22/libreelec-git-command/archive/refs/heads/main.zip
 KODI_DOCKER_ADDON_NAME=service.system.docker
-DOCKER_ADDON_PATH=~/.kodi/addons/$KODI_DOCKER_ADDON_NAME
-GIT_COMMAND_SOURCE=~/.git-command
+KODI_DOCKER_ADDON_PATH=~/.kodi/addons/$KODI_DOCKER_ADDON_NAME
+GIT_COMMAND_SOURCE=~/git-command-template
+TMP_INSTALL_DIR=/tmp/libreelec-git-command
 
-if [ ! -d "$DOCKER_ADDON_PATH" ]; then
+if [ ! -d "$KODI_DOCKER_ADDON_PATH" ]; then
   echo "The Docker service addon needs to be installed."
   echo "Please confirm the installation of the Docker addon or install manually."
   echo "Re-run this script after installation."
 
   # Try to install Docker service addon automatically.
-  kodi-send --action="InstallAddon(\"$KODI_DOCKER_ADDON_NAME\")"
+  kodi-send --action="InstallAddon(\"$KODI_DOCKER_ADDON_NAME\")" &>/dev/null
   exit 1
 fi
 
 # Download repository files and prepare for installation.
-mkdir -p /tmp/gitapp && cd "$_" || exit
-curl --location --remote-header-name --remote-name $REPO_FILE
-unzip libreelec-git-command-main.zip
-cd libreelec-git-command || exit
+mkdir -p $TMP_INSTALL_DIR && cd $TMP_INSTALL_DIR || exit
+wget -O libreelec-git-command-main.zip $REPO_FILE
+unzip -oq libreelec-git-command-main.zip
+cd libreelec-git-command-main || exit
 
 # Run actual install script.
+# TODO: Check whether docker command is available
 DOCKER_VOLUME_ID=$(docker build . | sed -n -e 's/^.*Successfully built //p')
 
 # Create the git alias function that will be sourced from ~/.profile.
-sed -e "s/GIT_DOCKER_ID=/GIT_DOCKER_ID=$DOCKER_VOLUME_ID" -- .git-command >$GIT_COMMAND_SOURCE
+sed -e "s/GIT_DOCKER_ID=/GIT_DOCKER_ID=$DOCKER_VOLUME_ID/" -- git-command-template >$GIT_COMMAND_SOURCE
 
 # Make git command available by sourcing newly created alias.
-grep -qxF 'source ~/.git-command' ~/.profile || echo 'source ~/.git-command' >>~/.profile
+grep -qxF "source $GIT_COMMAND_SOURCE" ~/.profile || echo "source $GIT_COMMAND_SOURCE" >>~/.profile
 source ~/.profile
 
 # Clean up all files after installation.
-rm -rf /tmp/gitapp
+rm -rf $TMP_INSTALL_DIR
 
-echo "Installation finished. You can now use the git command!"
+echo "Installation finished."
+echo "Please run 'source ~/.profile' or log off and on again once to use the git command!"
