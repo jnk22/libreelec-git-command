c03584d13907828a1a25f5385229d81556286096
Jannik Schäfer <jannik.schaefer@inbox-online.de>
2022-12-29 13:22:53 +0100

    Replace tabs with 2 spaces

--- install-git.sh
+++ install-git.sh
@@ -23,25 +23,25 @@ trap 'rm -rf -- "$TMP_DIR"' EXIT
 #   None
 #######################################
 main() {
-	echo "Verifying that system is supported..."
-	if ! check_system_supported; then
-		failed_abort "This script only supports LibreELEC."
-	fi
+  echo "Verifying that system is supported..."
+  if ! check_system_supported; then
+    failed_abort "This script only supports LibreELEC."
+  fi
 
-	echo "Verifying that '$KODI_DOCKER_ADDON_NAME' is installed and 'docker' command is available..."
-	if ! check_docker_command_available; then
-		echo "Addon not installed. Installing addon now. This may take a while..."
-		install_docker_addon
-	fi
+  echo "Verifying that '$KODI_DOCKER_ADDON_NAME' is installed and 'docker' command is available..."
+  if ! check_docker_command_available; then
+    echo "Addon not installed. Installing addon now. This may take a while..."
+    install_docker_addon
+  fi
 
-	echo "Downloading required files..."
-	download_required_files
+  echo "Downloading required files..."
+  download_required_files
 
-	echo "Building docker image and installing git command..."
-	install_docker_image
+  echo "Building docker image and installing git command..."
+  install_docker_image
 
-	echo "Installation finished!"
-	echo "Please run 'source ~/.profile' or reconnect once to use the git command."
+  echo "Installation finished!"
+  echo "Please run 'source ~/.profile' or reconnect once to use the git command."
 }
 
 #######################################
@@ -52,10 +52,10 @@ main() {
 #   error_msg
 #######################################
 failed_abort() {
-	local error_msg=$1
+  local error_msg=$1
 
-	echo "INSTALLATION FAILED: $error_msg"
-	exit 1
+  echo "INSTALLATION FAILED: $error_msg"
+  exit 1
 }
 
 #######################################
@@ -68,7 +68,7 @@ failed_abort() {
 #   0 if supported, 1 otherwise
 #######################################
 check_system_supported() {
-	[[ $(grep ^NAME /etc/os-release | cut -d '=' -f 2 | sed "s/\"//g") == "LibreELEC" ]] || return 1
+  [[ $(grep ^NAME /etc/os-release | cut -d '=' -f 2 | sed "s/\"//g") == "LibreELEC" ]] || return 1
 }
 
 #######################################
@@ -81,7 +81,7 @@ check_system_supported() {
 #   0 if available, 1 otherwise
 #######################################
 check_docker_command_available() {
-	command -v "$DOCKER_BIN_PATH" &>/dev/null || return 1
+  command -v "$DOCKER_BIN_PATH" &>/dev/null || return 1
 }
 
 #######################################
@@ -93,18 +93,18 @@ check_docker_command_available() {
 #   None
 #######################################
 install_docker_addon() {
-	kodi-send --action="InstallAddon(\"$KODI_DOCKER_ADDON_NAME\")" &>/dev/null
-	kodi-send --action="Action(\"Left\")" &>/dev/null
-	kodi-send --action="Action(\"Select\")" &>/dev/null
+  kodi-send --action="InstallAddon(\"$KODI_DOCKER_ADDON_NAME\")" &>/dev/null
+  kodi-send --action="Action(\"Left\")" &>/dev/null
+  kodi-send --action="Action(\"Select\")" &>/dev/null
 
-	timeout_counter=0
-	until (check_docker_command_available) || [ "$timeout_counter" -ge "$DOCKER_INSTALL_TIMEOUT" ]; do
-		sleep 1
-		timeout_counter=$((timeout_counter + 1))
-	done
+  timeout_counter=0
+  until (check_docker_command_available) || [ "$timeout_counter" -ge "$DOCKER_INSTALL_TIMEOUT" ]; do
+    sleep 1
+    timeout_counter=$((timeout_counter + 1))
+  done
 
-	check_docker_command_available ||
-		failed_abort "Could not install addon. Please install addon manually and try again."
+  check_docker_command_available ||
+    failed_abort "Could not install addon. Please install addon manually and try again."
 }
 
 #######################################
@@ -116,10 +116,10 @@ install_docker_addon() {
 #   None
 #######################################
 download_required_files() {
-	local zip_file_path="$TMP_DIR/$REPO_FILE_NAME.zip"
+  local zip_file_path="$TMP_DIR/$REPO_FILE_NAME.zip"
 
-	wget -q -O "$zip_file_path" "$REPO_FILE_URL"
-	unzip -oq "$zip_file_path" -d "$TMP_DIR"
+  wget -q -O "$zip_file_path" "$REPO_FILE_URL"
+  unzip -oq "$zip_file_path" -d "$TMP_DIR"
 }
 
 #######################################
@@ -134,12 +134,12 @@ download_required_files() {
 #   None
 #######################################
 install_docker_image() {
-	local repo_dir="$TMP_DIR/$REPO_FILE_NAME"
-	local docker_volume_id
-	docker_volume_id=$(command "$DOCKER_BIN_PATH" build "$repo_dir" | sed -n -e 's/^.*Successfully built //p')
+  local repo_dir="$TMP_DIR/$REPO_FILE_NAME"
+  local docker_volume_id
+  docker_volume_id=$(command "$DOCKER_BIN_PATH" build "$repo_dir" | sed -n -e 's/^.*Successfully built //p')
 
-	sed -e "s/GIT_DOCKER_ID=/GIT_DOCKER_ID=$docker_volume_id/" -- "$repo_dir/git-command-template" >"$GIT_COMMAND_PATH"
-	grep -qxF "source $GIT_COMMAND_PATH" "$PROFILE_PATH" &>/dev/null || echo "source $GIT_COMMAND_PATH" >>"$PROFILE_PATH"
+  sed -e "s/GIT_DOCKER_ID=/GIT_DOCKER_ID=$docker_volume_id/" -- "$repo_dir/git-command-template" >"$GIT_COMMAND_PATH"
+  grep -qxF "source $GIT_COMMAND_PATH" "$PROFILE_PATH" &>/dev/null || echo "source $GIT_COMMAND_PATH" >>"$PROFILE_PATH"
 }
 
 main
